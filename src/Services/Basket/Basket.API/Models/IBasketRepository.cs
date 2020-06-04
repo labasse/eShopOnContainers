f7c707cd6004876f09d5ce2fd506f6f90bf423b0
S.Labasse <labasse@gmail.com>
2020-06-04 15:15:35 +0200

    Initialization of BasketAPI project

---
+++ src/Services/Basket/Basket.API/Models/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Models
+{
+    public interface IBasketRepository
+    {
+        CustomerBasket GetBasketAsync(string customerId);
+        
+        CustomerBasket UpdateBasketAsync(CustomerBasket basket);
+        bool DeleteBasketAsync(string id);
+    }
+}
